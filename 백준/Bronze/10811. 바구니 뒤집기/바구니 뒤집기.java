9d11955d18709bf6e4e07521184336de9b9af280
EricNaKor <122855875+EricNakor@users.noreply.github.com>
2024-03-26 19:58:47 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+   
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st = new StringTokenizer( br.readLine() );
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
+  
+      int[] num = new int[N];
+      
+      //바구니 번호 저장
+      for(int i = 0; i < N;i++) {
+    	  num[i] = i+1;
+      }
+      
+      //바구니의 순서 역순으로 만들기
+      for(int i = 0; i < M;i++) {
+    	  st = new StringTokenizer(br.readLine());
+    	  int A = Integer.parseInt(st.nextToken());
+    	  int B = Integer.parseInt(st.nextToken());
+    	  int[] copy_num = Arrays.copyOfRange(num, A-1, B);
+    	  int c = copy_num.length;
+    	  for(int j = A-1; j < B; j++)
+    	  {
+    		num[j] = copy_num[c-1];
+    		c--;
+    	  }
+      }
+      
+      for(int j = 0; j < N;j++) {
+    	  bw.write(num[j] + " ");
+      }
+      
+      bw.flush();
+      bw.close();
+      br.close();
+   }   
+}
